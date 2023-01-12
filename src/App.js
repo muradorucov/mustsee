a6f82d5fddb9b21e030888c7bfead7aefd2121ae
muradorucov <orucowmurad@gmail.com>
2023-01-12 13:34:41 +0400

    feat: class component to function component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage/MainPage';
 import ListPage from './pages/ListPage/ListPage';
 
 import './reset.css';
 import './common.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app">
-        <Route path="/" exact component={MainPage} />
-        <Route path="/list/:id" exact component={ListPage} />
-      </div>
-    );
-  }
+
+const App = () => {
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/list/:id" element={<ListPage />} />
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
+
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-ReactDOM.render(
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+      <App />
   </BrowserRouter>,
-  document.getElementById('root')
-);
+)
