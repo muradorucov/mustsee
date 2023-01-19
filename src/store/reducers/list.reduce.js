70062284b785e795e103899f7a8b2e1320f7c153
muradorucov <orucowmurad@gmail.com>
2023-01-19 05:17:31 +0400

    fixed: listreducer two identical data are not added

--- src/store/reducers/list.reduce.js
+++ src/store/reducers/list.reduce.js
@@ -1,4 +1,10 @@
 export const listReduce = (state = [], action) => {
-    if (action.type === 'ADD-TO_LIST') return [...state, action.payload]
+    if (action.type === 'ADD-TO_LIST') {
+        if (!state.find(item => item.imdbID === action.payload.imdbID)) {
+            return [...state, action.payload]
+        }
+        return state
+
+    }
     return state
 }
