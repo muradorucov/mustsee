7c0f6ca22cf7ffa39297896aff3fd4b5e2f7513e
muradorucov <orucowmurad@gmail.com>
2023-01-24 06:10:26 +0400

    feat: api data fetch list page

--- src/pages/ListPage/ListPage.js
+++ src/pages/ListPage/ListPage.js
@@ -1,43 +1,57 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-// import { useParams } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+// import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import './ListPage.css';
 
 
 const ListPage = () => {
-    // const [movieId, setMovieId] = useState([])
-    // const [stateMovies, setstateMovies] = useState([])
-    const { list } = useSelector(state => state)
-    // let { id } = useParams();
+    const [algoApiData, setAlgoApiData] = useState({})
+    const [movie, setMovie] = useState([])
+    const [status, setStatus] = useState(false)
+    // const { list } = useSelector(state => state)
+    let { id } = useParams();
+    useEffect(() => {
+        fetch(`https://acb-api.algoritmika.org/api/movies/list/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                setAlgoApiData(data)
+            })
+    }, [id])
 
-    // useEffect(() => {
-    //     fetch(`https://acb-api.algoritmika.org/api/movies/list/${id}`)
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             setMovieId([...data.movies])
-    //             console.log(movieId)
-    //             // console.log(data.movies);
-    //             // data.movies.forEach(element => {
-    //             //     fetch(`https://www.omdbapi.com/?i=${element}&apikey=278924d5`)
-    //             //     .then(res => res.json())
-    //             //     .then(movie => {
-    //             //         setstateMovies([...stateMovies, { ...movie }])
-    //             //         console.log(movie);
-    //             //         console.log(stateMovies);
-    //             //     })
-    //             // });
+    let arr = []
 
-    //         })
-    // },[])
+    useEffect(() => {
+        if (algoApiData) {
+            Promise.all(algoApiData?.movies?.map((item) => fetch(`https://www.omdbapi.com/?i=${item}&apikey=278924d5`)))
+                .then(products => {
+                    products.forEach(element => {
+                        element.json().then(data => {
+                            if (data) {
+                                console.log(data)
+                                arr.push(data)
+                                setMovie(arr)
 
+                                console.log(movie)
+                                setStatus(true)
+                            } else {
+                                console.log("data hele gelmeyib")
+                            }
+
+                        })
+                    })
+                })
+                .catch((error) => console.log(error));
+        }
+    }, [algoApiData])
 
 
     return (
         <div className="list-page">
-            <h1 className="list-page__title">My list</h1>
-
-            <div className="row">
-                {list?.map((item) => (
+            {algoApiData ? <h1 className="list-page__title">{algoApiData.title}</h1> :
+                <h1 className="list-page__title">My list</h1>}
+            {status ? <div className="row">
+                {movie?.map((item) => (
                     <div className='list-movie-item' key={item.imdbID}>
                         {item.Poster === "N/A" ? <img src="https://media.comicbook.com/files/img/default-movie.png"
                             alt={item.Title} />
@@ -47,7 +61,9 @@ const ListPage = () => {
                     </div>
                 ))}
 
-            </div>
+            </div> : <span>loading ...</span>}
+
+
         </div>
     )
 }
