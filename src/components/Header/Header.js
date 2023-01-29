d78461ce8bf8a283a7728b8125a078064dcb80fc
muradorucov <orucowmurad@gmail.com>
2023-01-29 23:32:30 +0400

    feat: change menu desing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
-import React  from 'react';
+import React from 'react';
+import { Search2Icon } from '@chakra-ui/icons';
 import './Header.css';
 
 
@@ -7,7 +8,15 @@ const Header = () => {
         <header className="header">
             <h1 className="header__title">
                 MustSee
+                <div className='proggresBar'></div>
             </h1>
+            <ul className='list'>
+                <li>Movies</li>
+                <li>My lists</li>
+                <li>
+                    <Search2Icon />
+                </li>
+            </ul>
         </header>
     )
 }
