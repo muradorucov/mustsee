f1baf74978f5baadf66ab739ae1109ba1d00bb28
muradorucov <orucowmurad@gmail.com>
2023-02-05 02:41:03 +0400

    fixed: fav_list btn bug fixed

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -7,12 +7,9 @@ import { listIsEmpty, removeToList } from '../../store/actions/action';
 import './Favorites.css';
 
 const Favorites = () => {
-    let arr = []
     const [listName, setListName] = useState("");
-    const [btnStatus, setBtnStatus] = useState(false)
     const [saveList, setSaveList] = useState(false)
     const [data, setData] = useState();
-    const [items, setItems] = useState([]);
 
     const { list } = useSelector(state => state)
 
@@ -27,9 +24,6 @@ const Favorites = () => {
             title: listName,
             movies: list
         }
-        arr.push(listObj)
-        console.log(arr);
-        localStorage.getItem('items', JSON.stringify(arr));
 
         fetch('https://63c190e499c0a15d28ed39de.mockapi.io/api/v1/movies/', {
             method: 'POST',
@@ -39,7 +33,6 @@ const Favorites = () => {
             .then(res => res.json())
             .then(apiData => {
                 setData(apiData)
-                setItems([apiData])
             })
             .finally(() => {
                 dispatch(listIsEmpty())
@@ -49,7 +42,6 @@ const Favorites = () => {
                     setSaveList(false)
                 }, 2000)
             })
-        setBtnStatus(true)
     }
 
 
@@ -62,15 +54,16 @@ const Favorites = () => {
                 value={listName}
                 onChange={(e) => { setListName(e.target.value) }}
             />
-            {/* {data ? <Link to={`listdetail/${data.id}`} >
-                <button className="favorites__save">Go to list : {data.title}</button>
-            </Link> : null
-            } */}
             <button
                 type="button"
                 className="favorites__save"
                 onClick={getSaveList}
-                disabled={!listName || !list.length || btnStatus}>Save list</button>
+                disabled={!listName || !list.length}>Save list</button>
+            {data ? <Link to={`listdetail/${data.id}`} >
+                <button className="favorites__save">Go to list : {data.title}</button>
+            </Link> : null
+            }
+            
             <ul className="favorites__list">
                 {list?.map((item) => (<li key={item?.imdbID} className="list-item">
                     <span>{item?.Title} ({item?.Year})</span>
