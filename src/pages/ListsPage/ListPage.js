455cc9f557ab3a49decd30520ae43b92e7626630
muradorucov <orucowmurad@gmail.com>
2023-02-04 05:16:23 +0400

    feat: my list add to localStorage

--- src/pages/ListsPage/ListPage.js
+++ src/pages/ListsPage/ListPage.js
@@ -1,10 +1,26 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import './ListPage.css'
 export const ListPage = () => {
+    const [list,setList] = useState(null)
+
+    useEffect(() => {
+        const localValue = JSON.parse(localStorage.getItem("mylist"));
+        if (localValue) {
+            setList([...localValue])
+        }
+    }, []);
     return (
-        <>
+        <div className='listpage'>
             <div>ListPage</div>
 
-        </>
+            <ul>
+                {list?.map(item => (
+                    <>
+                        <li key={item.id}>{item.title}</li>
+                    </>
+                ))}
+            </ul>
+
+        </div>
     )
 }
