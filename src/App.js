b5bc29b42139b42705a8cbba7689941715ec8e20
muradorucov <orucowmurad@gmail.com>
2023-02-13 17:05:13 +0400

    feat: bug fixed -netlify

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { Route, Routes } from 'react-router-dom';
 import MainPage from './pages/main';
 import { ListPage } from './pages/listpage';
 import { ListPageDetail } from './pages/listdetail';
-// import { NotFound } from './pages/notfound';
-import { MovieDetail } from './pages/moviedetail';
-
+import { MovieDetail } from './pages/moviedetail/MovieDetail';
+import { NotFound } from './pages/notfound/NotFound';
 import './reset.css';
 import './common.css';
+
 
 const App = () => {
   return (
@@ -17,7 +17,7 @@ const App = () => {
         <Route path="/lists" element={<ListPage />} />
         <Route path="/listdetail/:id" element={<ListPageDetail />} />
         <Route path="/movie/:id" element={<MovieDetail />} />
-        {/* <Route path="/*" element={< NotFound />} /> */}
+        <Route path="/*" element={< NotFound />} />
       </Routes>
     </div>
   )
--- src/pages/moviedetail/MovieDetail.js
+++ src/pages/moviedetail/MovieDetail.js
@@ -13,7 +13,7 @@ export const MovieDetail = () => {
     }, [id])
     return (
         <>
-        <Header/>
+            <Header />
         </>
     )
 }
